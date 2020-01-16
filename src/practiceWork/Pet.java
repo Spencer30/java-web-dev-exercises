c43671c3edb0c1156b559148f848430fd3a005d6
Spencer Lyons <sjl8r8@gmail.com>
2020-01-15 18:59:42 -0600

    added in unit testing and menu studio

---
+++ src/practiceWork/Pet.java
@@ -0,0 +1,22 @@
+package practiceWork;
+
+public class Pet {
+
+    private String name;
+
+    Pet(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    String firstPet = "Fluffy";
+    Pet secondPet = new Pet("Fluffy");
+    Pet thirdPet = new Pet("Fluffy");
+
+
+}
+
+
